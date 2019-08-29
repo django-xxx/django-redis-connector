9df384f8d57c501afe9e298072a2f0216c0294a9
Brightcells <brightcells@gmail.com>
2019-08-30 04:46:58 +0800

    :art: Support arg decode_responses

--- django_redis_connector/redis_connector.py
+++ django_redis_connector/redis_connector.py
@@ -4,13 +4,13 @@ import redis_extensions as redis
 
 
 def redis_conf(conf):
-    conf.update({
+    return {
         'host': conf.get('HOST', 'localhost'),
         'port': conf.get('PORT', 6379),
         'password': '{}:{}'.format(conf.get('USER', ''), conf.get('PASSWORD', '')) if conf.get('USER') else '',
         'db': conf.get('db', 0),
-    })
-    return conf
+        'decode_responses': conf.get('decode_responses', False),
+    }
 
 
 def connector(conf):
